e16b304dbd390d8785376217d660f9b9453a49a8
alanprates <alan_nz@live.com>
2023-12-03 22:34:50 -0300

    Classe Exercicio

---
+++ SistemaGerenciamentoAcademia/Exercicio.cs
@@ -0,0 +1,8 @@
+public class Exercicio
+{
+    public string GrupoMuscular { get; set; }
+    public int Series { get; set; }
+    public int Repeticoes { get; set; }
+    public int TempoIntervaloSegundos { get; set; }
+    public object Nome { get; internal set; }
+}
