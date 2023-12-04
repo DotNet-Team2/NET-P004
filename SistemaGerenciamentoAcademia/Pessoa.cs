7dd38fec9b2d3a5396d904ebd7b1bf1d033b3b9b
alanprates <alan_nz@live.com>
2023-12-03 22:32:40 -0300

    Classe Pessoa

---
+++ SistemaGerenciamentoAcademia/Pessoa.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class Pessoa
+{
+    public string Nome { get; set; }
+    public DateTime DataNascimento { get; set; }
+    public string CPF { get; set; }
+
+    public Pessoa(string nome, DateTime dataNascimento, string cpf)
+    {
+        Nome = nome;
+        DataNascimento = dataNascimento;
+        CPF = cpf;
+    }
+}
