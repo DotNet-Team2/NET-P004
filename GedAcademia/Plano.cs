1ca177fea55fa7ddd6057a250f65d2a41fb092ca
Alessandro Conceição Santos <alessanbaixista@gmail.com>
2023-12-05 15:43:08 -0300

    Upgrade classes app + Plano + Program

--- GedAcademia/Plano.cs
+++ GedAcademia/Plano.cs
@@ -1,7 +1,19 @@
 public class Plano {
-    public string título;
+    public string? titulo;
     private double valorPorMes;
 
+    public Plano (string titulo, double valor){
+        this.titulo = titulo;
+        this.valorPorMes = valor;
+    }
+
+    public string getTitulo(){
+        return this.titulo;
+    }
+
+    public double  getValorPorMes(){
+        return this.valorPorMes;
+    }
     public double ValorPorMes {
         get { return valorPorMes; }
         set { valorPorMes = value; }
