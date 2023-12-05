97f23fee5e1d96a660c483ec4668965f5233f55a
FranklinMenezes2309 <fmpereira2309@hotmail.com>
2023-12-05 15:08:53 -0300

    feat: classe plano e pagamento

---
+++ GedAcademia/Plano.cs
@@ -0,0 +1,9 @@
+public class Plano {
+    public string título;
+    private double valorPorMes;
+
+    public double ValorPorMes {
+        get { return valorPorMes; }
+        set { valorPorMes = value; }
+    }
+}
