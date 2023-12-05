e43fcb967ce18166632c74407794a2553dbfbee1
“danielcoutinhoneto” <danielcoutinhoneto@outlook.com>
2023-12-05 17:00:39 -0300

    Implementação de Forma de Pagamento

---
+++ GedAcademia/PagCartao.cs
@@ -0,0 +1,12 @@
+
+namespace GedAcademia
+{
+    public class PagCartao : Pagamento
+    {
+        public string NumeroCartao { get; set; }
+        public void RealizarPagamento(double valor)
+        {
+            Console.WriteLine($"Pagamento de {valor} realizado com cartão de crédito");
+        }
+    }
+}
