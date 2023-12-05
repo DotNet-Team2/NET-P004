202f5a177bcb43e1ff64df63c12a0aed8aef2ed4
Vitor Sousa <vitor.pereira@cepedi.org.br>
2023-12-05 16:27:07 -0300

    Implementação de entrada de planos

--- GedAcademia/Cliente.cs
+++ GedAcademia/Cliente.cs
@@ -4,5 +4,7 @@ public class Cliente : Pessoa
 {
     public int AlturaCm { get; set; }
     public int PesoKg { get; set; }
+    public Plano planoAtivo;
+    List<Pagamento>pagamentosclientes =new List<Pagamento>();
     public List<Avaliacao> Avaliacoes { get; set; } = new List<Avaliacao>();
 }
